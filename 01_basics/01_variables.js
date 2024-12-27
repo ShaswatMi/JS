3941ff3d1f1e85c1c5f41379bb3e362e57072f4c
ShaswatMi <shaswat.mishra81@gmail.com>
2024-12-27 09:48:23 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 122553
+let accountEmail="shaswat@gmail.com"
+var accountPassword ="12345"
+accountcity="tumkur"
+let accountState;
+
+// accountId=23 not allowed to change const
+
+accountEmail="sm@gmail.com"
+accountPassword="12543"
+accountcity = "benguluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
+
+/*
+prefer not to use var use let instead
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js as newer version
+
+// alert(3+3) we use node js not browser
+
+console.log(3+3)
+
+let name ="Shaswat"
+let age = 18
+let isLoggedIn = false
+
+//primitive
+// number =>2 to power 53
+// bigint
+// string => "" 
+// boolean =>true/false
+// null =>standalone value
+// undefined => value not assigned
+// symbol => unique
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
