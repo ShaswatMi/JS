30d9cfab3ee2201e3902c20e7820939586552e4a
ShaswatMi <shaswat.mishra81@gmail.com>
2024-12-27 10:35:06 +0000

    Comparisons

--- 01_basics/03_convertionsOperations.js
+++ 01_basics/03_convertionsOperations.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=? true; 0=>false
 // "" =>false
@@ -23,4 +23,33 @@ console.log(booleanIsLoggedIn);
 let somenumber=33
 
 let stringnumber=String(somenumber);
-console.log(typeof stringnumber);
+//console.log(typeof stringnumber);
+
+//           operations           
+
+let value=3
+let negvalue=-value
+//console.log(negvalue);
+
+let str1="hello"
+let str2=" shaswat"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+"2"); //12
+// console.log("1"+2+2); //here 122 comes
+// console.log(1+2+"2"); //here 32 comes
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,15 @@
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
+/*Here equality and comparison operatpr are different from each other. comparisons convert null to a number , 
+ treating it as 0 hence third one is true and other two are false*/
+
+ console.log(undefined >=0); //false
+
+ // === it checks strictly
+
+ console.log("2"===2);
+ //we avoid these type of codes
+ 
+ 
+
